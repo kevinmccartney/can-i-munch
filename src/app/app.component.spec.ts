c79a75272f93a88ca89be13d86928a8ae226feab
Kevin McCartney <hello@kevinmccartney.is>
2020-01-09 01:14:30 -0500

    Fixing tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,17 +2,27 @@ import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { SharedModule } from '@modules/shared';
+import { AmplifyService } from 'aws-amplify-angular';
+import { RouterModule } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule,
         SharedModule,
+        RouterModule.forRoot([]),
       ],
       declarations: [
         AppComponent
       ],
+      providers: [
+        {
+          provide: AmplifyService,
+          useValue: {
+            auth: () => {},
+          }
+        }
+      ]
     }).compileComponents();
   }));
 
